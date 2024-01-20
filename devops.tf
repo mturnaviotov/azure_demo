ba72615e9d93eb22575d71518ece411f266f6ce9
Mykola Turnaviotov <turnaviotovnn@gmail.com>
2024-01-20 23:26:43 +0100

    Service endpoint and pipeline auth updated, cosmetic clearance

--- devops.tf
+++ devops.tf
@@ -4,18 +4,6 @@ resource "azuredevops_project" "blog" {
   work_item_template = "Agile"
 }
 
-# Default repo is unusable by terraform
-# data "azuredevops_git_repository" "blog-default" {
-#   project_id = azuredevops_project.blog.id
-#   name       = "blog"
-# }
-
-# resource "azuredevops_git_repository_branch" "blog-default" {
-#   repository_id = data.azuredevops_git_repository.blog-default-repo.id
-#   name          = "main"
-#   ref_branch    = "main" #data.azuredevops_git_repository.blog.default_branch
-# }
-
 #terraform import azuredevops_git_repository.blog blog/blog-app
 resource "azuredevops_git_repository" "blog" {
   name           = "${var.appname}-app"
@@ -28,15 +16,7 @@ resource "azuredevops_git_repository" "blog" {
   lifecycle {
     ignore_changes = [initialization]
   }
-
 }
-
-# main branch already created by Azure (auto branch creation policy on organization level)
-# resource "azuredevops_git_repository_branch" "blog-main" {
-#   repository_id = azuredevops_git_repository.blog.id
-#   name          = "main"
-#   ref_branch    = "refs/heads/main" #data.azuredevops_git_repository.blog.default_branch
-# }
 
 resource "azuredevops_git_repository_file" "readme-zero" {
   repository_id       = azuredevops_git_repository.blog.id
@@ -58,8 +38,14 @@ resource "azuredevops_git_repository_file" "readme-zero" {
 resource "azuredevops_git_repository_file" "default_pipeline" {
   repository_id = azuredevops_git_repository.blog.id
   file          = "azure-pipelines.yml"
-  content = templatefile("./azure-pipelines.tftpl", { appname = var.appname, registry_uri = azurerm_container_registry.blog.login_server, endpoint_id = azuredevops_serviceendpoint_dockerregistry.blog.id,
-  subscription = "${var.subscription_name}(${var.subscription_id})", name = "${var.appname}${random_id.app.hex}" })
+
+  content = templatefile("./azure-pipelines.tftpl", {
+    registry_uri = azurerm_container_registry.blog.login_server,
+    agent_pool   = var.agent_pool,
+    cr_id        = azuredevops_serviceendpoint_azurecr.blog.service_endpoint_name,
+    subscription = azuredevops_serviceendpoint_azurerm.blog.service_endpoint_name,
+    name         = "${var.appname}${random_id.app.hex}"
+  })
 
   branch              = "refs/heads/main"
   commit_message      = "Add azure-pipelines.yml"
@@ -104,6 +90,10 @@ resource "azuredevops_pipeline_authorization" "blog" {
   resource_id = data.azuredevops_agent_queue.azure.id
   type        = "queue"
   pipeline_id = azuredevops_build_definition.blog.id
+
+  lifecycle {
+    ignore_changes = [resource_id]
+  }
 }
 
 resource "azuredevops_serviceendpoint_azurerm" "blog" {
@@ -113,4 +103,5 @@ resource "azuredevops_serviceendpoint_azurerm" "blog" {
   azurerm_spn_tenantid                   = var.tenant_id
   azurerm_subscription_id                = var.subscription_id
   azurerm_subscription_name              = var.subscription_name
+  resource_group                         = azurerm_resource_group.blog.name
 }
