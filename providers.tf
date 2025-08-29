65cf7d5751e278c8f8f5333b66306f00f06ad64a
Mykola Turnaviotov <turnaviotovnn@gmail.com>
2025-08-29 19:29:10 +0200

    version bump

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.87.0"
+      version = "4.42.0"
     }
     azuredevops = {
       source  = "microsoft/azuredevops"
-      version = ">= 0.11.0"
+      version = "1.11.2"
     }
   }
 }
